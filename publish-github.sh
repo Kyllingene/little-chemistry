0e734862d900e0d8e7253e13e428f42aceade41e
Kyllingene <fuzzmuffin343@gmail.com>
2023-05-17 20:34:07 -0700

    Update publish script

--- publish-github.sh
+++ publish-github.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -ex
 
+echo "Building WASM package..."
+cargo build --release --features=gui --target=wasm32-unknown-unknown
+
+echo "Running Github deploy script..."
 set current_branch $(git branch --show-current)
 git checkout gh-pages
 
 ./publish.sh
 
 git checkout "$current_branch"
+
+echo "Done!"
